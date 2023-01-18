ba7bc9f289f012e0ff18e18b3edcdc9bf78b8f31
Taylor Layton <122323403+laytonoid@users.noreply.github.com>
2023-01-17 21:20:39 -0600

    Add files via upload

---
+++ Controller/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Responsive.Models;
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.Name = "Taylor";
+        ViewBag.BirthYear = 2000;
+        ViewBag.FV = 0;
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(ResponsiveModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            ViewBag.FV = model.CalculateAge();
+        }
+        else
+        {
+            ViewBag.FV = 0;
+        }
+        return View(model);
+    }
+}
---
+++ Models/ResponsiveModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Responsive.Models
+{
+    public class ResponsiveModel
+    {
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+
+        public string CalculateAge()
+        {
+            int currentYear = 2023;
+            string age = Convert.ToString(currentYear - BirthYear);
+            string finalValue = Name + " is " + age + ".";
+            return finalValue;
+        }
+    }
+}
