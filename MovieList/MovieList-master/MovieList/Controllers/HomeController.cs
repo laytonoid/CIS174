45cad43701f3d081405f47781381db5f219223a6
Taylor Layton <tglayton@dmacc.edu>
2023-03-29 20:51:37 -0500

    Validation

--- MovieList/MovieList-master/MovieList/Controllers/HomeController.cs
+++ MovieList/MovieList-master/MovieList/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieList.Models;
@@ -30,6 +31,19 @@ namespace MovieList.Controllers
             var genre = query.ToList();
             return View(genres);
         }
+        public class Customer
+        {
+            [Required(ErrorMessage = "Please enter a name.")]
+            [RegularExpression("^[a-zA-Z0-9]+$",
+            ErrorMessage = "Name may not contain special characters.")]
+            public string Name { get; set; }
+            [Required(ErrorMessage = "Please enter a date of birth.")][Range(typeof(DateTime), "1/1/1900", "12/31/9999", ErrorMessage = "Date of birth must be after 1/1/1900.")] public DateTime? DOB { get; set; }
+            [Required(ErrorMessage = "Please enter a password.")]
+            [StringLength(25)]
+            [Compare("ConfirmPassword")] public string Password { get; set; }
+            [Required(ErrorMessage = "Please confirm your password.")][Display(Name = "Confirm Password")] public string ConfirmPassword { get; set; }
+        }
+
     }
 
 }
